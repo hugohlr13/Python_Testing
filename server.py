99e24fb3892edd77909598ff26ae694e291ace33
hugohlr13 <hugo.huetleroy@gmail.com>
2023-10-22 11:08:17 +0200

    Fix bug email-crash

--- server.py
+++ server.py
@@ -24,11 +24,13 @@ clubs = loadClubs()
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
-
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+    except IndexError:
+        return "Sorry, that email wasn't found.", 404
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
